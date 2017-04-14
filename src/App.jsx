7126d28c0ae4aab45a3380d4b869ae9f0466638a
Patrick Ocampo <pegnovi@yahoo.com>
2017-04-14 14:50:38 -0700

    Moved navBar logic into separate file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const selectedStyle = {
-  fontWeight: 'bold',
-  color: 'red'
-};
-
-const tVar = {
-  item: 'bob'
-};
+import CustomNavigationBar from './CustomNavigationBar';
 
 const Test = () => {
   return (
@@ -42,37 +34,12 @@ class App extends Component {
             <h2>Welcome to React</h2>
           </div>
 
-          <Navbar>
-            <Nav>
+          <CustomNavigationBar/>
 
-              <NavItem eventKey={1}>
-                <NavLink exact to="/about" activeStyle={selectedStyle}>About</NavLink>
-              </NavItem>
+          <Route path="/about" component={Test}/>
+          <Route path="/about2" component={Test}/>
+          <Route path="/test/:testText" component={TestParam}/>
 
-              <NavItem eventKey={2}>
-                <NavLink exact to="/about2" activeStyle={selectedStyle}>About2</NavLink>
-              </NavItem>
-
-            
-              <NavItem eventKey={3}>
-                <NavLink to={`/test/${tVar.item}`}>Test</NavLink>
-              </NavItem>
-            
-
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.4}>Separated link</MenuItem>
-              </NavDropdown>
-            </Nav>
-          </Navbar>
-
-
-           <Route path="/about" component={Test}/>
-           <Route path="/about2" component={Test}/>
-           <Route path="/test/:testText" component={TestParam}/>
         </div>
       </Router>
     );
