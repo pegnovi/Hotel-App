b60db53a8defcdc35043d6d97dde0ddb406cf67d
Patrick Ocampo <pegnovi@yahoo.com>
2017-04-14 23:56:55 -0700

    Created Express app. Added the following middleware into express app: -morgan -body-parser

--- server/app.js
+++ server/app.js
@@ -2,3 +2,21 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+const app = express();
+
+// middleware
+// Setup Logger
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version"' + ':status :res[content-length] :response-time ms'));
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// req body middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// backend routing
+
+
+module.exports = app;
